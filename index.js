9018af07a3bdc01c62026d076fb8842d7c2d1f99
Sourabh Singh <sourabhsingh01998@gmail.com>
2022-07-24 09:07:27 +0530

    Chapter details

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const chapertlist = require("./utils/chapters");
+const chapterdetails = require("./utils/chapterdetails");
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -9,13 +10,30 @@ app.use(express.json())
 
 
 app.get("/chapters", (req, res) =>{
-   chapertlist().then(function(data){
-      res.send(data);
-   });
+   try{ 
+      chapertlist().then(function(data){
+         res.send(data);
+      });
+   }catch(e){
+      res.status(400).send(e);
+   }
 })
 app.post("/chapterdetails", (req, res) =>{
-   res.send(req);
-})
+   const chapterlink = req.body.chapterlink;
+   try{
+      if(chapterlink != ""){
+         chapterdetails(chapterlink).then(function(data){
+            res.status(200).send(data);
+         }).catch((e)=>{
+            res.status(400).send({"error": e.originalMessage})
+         })
+      }else{
+         res.status(404).send({"error":"Chapter link is empty"});
+      }
+   }catch(e){
+      res.status(400).send(e);
+   }
+}) 
 
 
 app.get("", (req,res)=>{
