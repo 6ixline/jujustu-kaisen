7628efc5af0d44fd24969616dbd5ebef8ededd02
Sourabh Singh <sourabhsingh01998@gmail.com>
2022-07-24 10:22:40 +0530

    vercel File Add

--- utils/chapters.js
+++ utils/chapters.js
@@ -2,22 +2,22 @@ const pp = require('puppeteer');
 
 async function chapterList(){
     try{  
-        const brower = await pp.launch();
-        const page = await brower.newPage();
-        await page.goto('https://readkaisen.com/');
-        const data = await page.evaluate(function(){
-            const chapterLinks = document.querySelectorAll(".table tbody tr");
-            const array = [];
-            let episode = chapterLinks.length;
+        // const brower = await pp.launch();
+        // const page = await brower.newPage();
+        // await page.goto('https://readkaisen.com/');
+        // const data = await page.evaluate(function(){
+        //     const chapterLinks = document.querySelectorAll(".table tbody tr");
+        //     const array = [];
+        //     let episode = chapterLinks.length;
 
-            for(i=0; i< chapterLinks.length; i++){
-                array.push('https://readkaisen.com' + chapterLinks[i].querySelectorAll("td a")['1'].getAttribute("href"));
-                episode--;
-            }
-            return array
-        })
+        //     for(i=0; i< chapterLinks.length; i++){
+        //         array.push('https://readkaisen.com' + chapterLinks[i].querySelectorAll("td a")['1'].getAttribute("href"));
+        //         episode--;
+        //     }
+        //     return array
+        // })
 
-        return data;
+        return {"TITLE" : "TY"};
     }catch(e){
         console.log("Error! Something wrong")
     }
