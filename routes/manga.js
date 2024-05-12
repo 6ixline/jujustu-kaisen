1fd91df70482788f998d565e09f96d3734d21ca6
Sourabh Singh <sourabhsingh01998@gmail.com>
2024-05-12 10:59:54 +0530

    add cache

--- routes/manga.js
+++ routes/manga.js
@@ -1,21 +1,27 @@
 const express = require('express');
 const chapertlist = require("../utils/chapters");
 const chapterdetails = require("../utils/chapterdetails");
+const NodeCache = require( "node-cache" );
 const Category = require("../models/category");
 const Manga = require("../models/manga");
-
+const myCache = new NodeCache();
 const router = new express.Router();
 
 // Get All Manga data
 router.get("/mangadata", async (req, res) => {
    try {
-      let data = [];
-      const categories = await Category.find({status: 'active'}).sort({ order: -1 });
-      for (const element of categories) {
-         let mangaData = await Manga.find({ category: element._id, 'status': 'active' }).sort({order: 1})
-         data.push({ "title": element.title, "manga": mangaData })   
+      let mangaData = myCache.get("allmanga");
+      if(mangaData == undefined){
+         let data = [];
+         const categories = await Category.find({status: 'active'}).sort({ order: -1 });
+         for (const element of categories) {
+            let mangaData = await Manga.find({ category: element._id, 'status': 'active' }).sort({order: 1})
+            data.push({ "title": element.title, "manga": mangaData })   
+         }
+         mangaData = data;
+         myCache.set("allmanga", data, 86400);
       }
-      res.status(200).send(data);
+      res.status(200).send(mangaData);
 
    } catch (e) {
       res.status(400).send(e);
--- routes/mangaDetails.js
+++ routes/mangaDetails.js
@@ -1,14 +1,30 @@
 const express = require("express");
+const crypto = require("crypto");
+const NodeCache = require( "node-cache" );
 const chapertlist = require("../utils/chapters");
 const chapterdetails = require("../utils/chapterdetails");
+const myCache = new NodeCache();
 
 const router = express.Router();
+const secret = process.env.SECRET;
 
+function getKey(val){
+    const hash =  crypto.createHash("sha256");
+    hash.update(val);
+    val = hash.digest("hex");
+    return val;
+}
 
 router.post("/chapterlists", async(req,res)=>{
     try {
-        const data = await chapertlist(`${req.body.mangaUrl}`, `${req.body.chapterRoute}`, `${req.body.chapterRouteEnd}`);
-        res.status(200).send(data);
+        let key = getKey(req.body.mangaUrl);
+        let chapterlist = myCache.get(key);
+        if(chapterlist == undefined){
+            const data = await chapertlist(`${req.body.mangaUrl}`, `${req.body.chapterRoute}`, `${req.body.chapterRouteEnd}`);
+            myCache.set(key, data, 86400);
+            chapterlist = data;
+        }
+        res.status(200).send(chapterlist);
      } catch (e) {
         res.status(400).send(e);
      }
@@ -17,11 +33,20 @@ router.post("/chapterdata", async(req,res)=>{
     const chapterlink = req.body.chapterlink;
     try {
         if (chapterlink != "") {
-            chapterdetails(chapterlink, req.body.chapterImageEnd, req.body.imageDirect, req.body.scroll).then(function (data) {
-                res.status(200).send(data);
-            }).catch((e) => {
-                res.status(400).send({ "error": e.originalMessage })
-            })
+            let key = getKey(chapterlink);
+            let chapterData = myCache.get(key);
+            if(chapterData == undefined){
+                chapterdetails(chapterlink, req.body.chapterImageEnd, req.body.imageDirect, req.body.scroll).then(function (data) {
+                    if(data){
+                        myCache.set(key, data, 86400);
+                    }
+                    res.status(200).send(data);
+                }).catch((e) => {
+                    res.status(400).send({ "error": e.originalMessage })
+                })
+            }else{
+                res.status(200).send(chapterData);
+            }
         } else {
             res.status(404).send({ "error": "Chapter link is empty" });
         }
